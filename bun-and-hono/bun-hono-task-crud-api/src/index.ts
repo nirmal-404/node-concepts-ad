047171e8d51f0eef1e2dea5c38c6325a7f096fec
Nirmal Perera <143857543+Manusha-Nirmal-Perera@users.noreply.github.com>
2025-04-25 16:33:25 +0530

    bun hono basic crud api

--- bun-and-hono/bun-hono-task-crud-api/src/index.ts
+++ bun-and-hono/bun-hono-task-crud-api/src/index.ts
@@ -1,9 +1,64 @@
 import { Hono } from 'hono'
+import { initDatabase } from './database/db'
+import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+import { loginUser, registerUser } from './controller/auth'
+import { jwt } from 'hono/jwt'
+import { createTask, deleteTask, getAllTasks, getTask, updateATask } from './controller/task'
 
 const app = new Hono()
+const db = initDatabase()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use('*', cors())
+app.use('*', logger())
+
+const auth = jwt({
+  secret: process.env.JWT_SECRET || 'JWT_SECRET'
+})
+
+// Input validation
+const registerSchema = z.object({
+  username: z.string().min(3).max(25),
+  password: z.string().min(5),
+  role: z.enum(['admin', 'user']).optional()
+})
+
+const loginSchema = z.object({
+  username: z.string(),
+  password: z.string()
+})
+
+const taskSchema = z.object({
+  title: z.string().min(1).max(100),
+  description: z.string().optional(),
+  user_id: z.number().int().positive()
+})
+
+// Authentication routes
+app.post('/register-user', zValidator('json', registerSchema), c =>
+  registerUser(c, db)
+)
+app.post('/login-user', zValidator('json', loginSchema), c => loginUser(c, db))
+
+// Task routes
+app.post('/tasks', auth, zValidator('json', taskSchema), c => createTask(c, db))
+app.get('/tasks', auth, c => getAllTasks(c, db))
+app.get('/tasks/:id', auth, c => getTask(c, db))
+app.put('/tasks/:id', auth, zValidator('json', taskSchema), c => updateATask(c, db))
+app.delete('/tasks/:id', auth, c => deleteTask(c, db))
+
+app.get('/', c => {
+  return c.text('Hello, User and task management using Bun & Hono')
+})
+
+app.get('/db-test', c => {
+  const result = db.query('SELECT sqlite_version()').get()
+  return c.json({
+    message: 'Database connection successful',
+    sqlite_version: result
+  })
 })
 
 export default app
